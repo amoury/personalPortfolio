766445df544f310216842eabb2ab9d4d0d74d44a
Amoury <ansar.memon@hotmail.com>
2016-09-21 09:36:29 +0400

    Version 2 - Accidentally bumped into Reveal Effect. Check the config ScrollMagic. A slide moves up to reveal another section under that.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+// Canvas Animation
+particlesJS("particles-js", {"particles":{"number":{"value":150,"density":{"enable":true,"value_area":1500.00}},"color":{"value":"#E6EBE0"},"shape":{"type":"star","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":8},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.38481889460772545,"random":false,"anim":{"enable":true,"speed":3.329516570435515,"opacity_min":0.17865698670629593,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":8.932849335314796,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#E6EBE0","opacity":0.7839692548078818,"width":2.367442924896818},"move":{"enable":true,"speed":3.810236182596568,"direction":"none","random":true,"straight":false,"out_mode":"bounce","bounce":false,"attract":{"enable":true,"rotateX":1443.0708547789707,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"grab"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":170.53621458328246,"line_linked":{"opacity":0.5264803145719884}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":119.88011988011989,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
 
+//ScrollMagic
+
+$(document).ready(function() {
+	// Init ScrollMagic
+	var controller = new ScrollMagic.Controller();
+
+	// Scene 1 - pin the first Portfolio section
+
+	var portfolio01 = new ScrollMagic.Scene({
+		triggerElement: '#portfolio01',
+		triggerHook: 0,
+		duration: '100%'
+	})
+	.setPin('#portfolio01')
+	.addTo(controller);
+});
